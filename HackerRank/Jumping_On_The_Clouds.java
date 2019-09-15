5ec371215ec0aa075bb4a078e7bf968ed282a8ec
Pavan Rajkumar <pavanraj14@yahoo.com>
2019-09-15 14:45:07 +0530

    Jumping on the Clouds

---
+++ HackerRank/Jumping_On_The_Clouds.java
@@ -0,0 +1,65 @@
+//https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the jumpingOnClouds function below.
+    static int jumpingOnClouds(int[] c) {
+        int jumps = 0;
+        int i = 0;
+
+        while(i<c.length-2){
+            if(c[i+2]==0){
+                jumps++;
+                i = i+2;
+                continue;
+            }
+            else if(c[i+1]==0){
+                jumps++;
+                i = i+1;
+            }
+        }
+        if(i==c.length-2){
+            if(c[i+1]==0)
+                jumps++;
+        }
+
+        return jumps;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] c = new int[n];
+
+        String[] cItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int cItem = Integer.parseInt(cItems[i]);
+            c[i] = cItem;
+        }
+
+        int result = jumpingOnClouds(c);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}

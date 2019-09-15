c73c4e7cc3de17a2f139c4cb9727b5541ae265fe
Pavan Rajkumar <pavanraj14@yahoo.com>
2019-09-15 17:24:47 +0530

    2D_Arrays_DS

---
+++ HackerRank/2D_Arrays_DS.java
@@ -0,0 +1,61 @@
+//https://www.hackerrank.com/challenges/2d-array/problem?h_l=interview&playlist_slugs%5B%5D%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D%5B%5D=arrays&isFullScreen=true
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the hourglassSum function below.
+    static int hourglassSum(int[][] arr) {
+        int sum = 0;
+        int sum_array[] = new int[16];
+        int max=-999999;
+
+        for(int i=0;i<4;i++){
+            for(int k=0;k<4;k++){
+                sum = arr[i][k]+arr[i][k+1]+arr[i][k+2]
+                +arr[i+1][k+1]
+                +arr[i+2][k]+arr[i+2][k+1]+arr[i+2][k+2];
+
+                max = Math.max(sum,max);
+            }
+        }
+
+
+        return max;
+
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int[][] arr = new int[6][6];
+
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+
+        int result = hourglassSum(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}

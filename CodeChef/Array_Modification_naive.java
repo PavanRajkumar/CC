296640f676544e40b11e118f70a61269853447a9
Pavan Rajkumar <pavanraj14@yahoo.com>
2019-10-07 12:59:06 +0530

    Add Array modification naive

---
+++ CodeChef/Array_Modification_naive.java
@@ -0,0 +1,36 @@
+//https://www.codechef.com/OCT19B/problems/MARM
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef{
+	public static void main(String args[]) {
+
+		try {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+
+		for(int t=0;t<T;t++) {
+			int N = sc.nextInt();
+			long K = sc.nextLong();
+			long A[] = new long[N];
+
+			for(int j=0;j<=N-1;j++)
+				A[j] = sc.nextInt();
+
+			for(long i=0;i<=K-1;i++) {
+				long r = i%N;
+				A[(int) (r)] = A[(int) (r)] ^ A[(int) (N-(r)-1)];
+			}
+
+			for(int k=0;k<=N-1;k++)
+				System.out.print(A[k] + " ");
+
+		}
+		}
+		catch(Exception e) {
+			//ignore
+		}
+	}
+}

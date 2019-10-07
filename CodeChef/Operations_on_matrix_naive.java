4cef47455069c20aba91fe08757f4be528c5c8f3
Pavan Rajkumar <pavanraj14@yahoo.com>
2019-10-07 06:07:33 +0530

    Improve operations on matrix

--- CodeChef/Operations_on_matrix_naive.java
+++ CodeChef/Operations_on_matrix_naive.java
@@ -1,11 +1,10 @@
-//https://www.codechef.com/OCT19B/problems/SAKTAN
 
 import java.util.*;
-
-public class Codechef {
+class Codechef {
 
 	public static void main(String args[]) {
 
+		try{
 		Scanner sc = new Scanner(System.in);
 
 		int T = sc.nextInt();
@@ -14,6 +13,8 @@ public class Codechef {
 
 			int N = sc.nextInt();
 			int M = sc.nextInt();
+
+			if(N<=300 & M<=300){
 			int arr[][] = new int[N][M];
 
 			int Q = sc.nextInt();
@@ -33,8 +34,38 @@ public class Codechef {
 				}
 			}
 			System.out.println(count);
+			}
+			else{
+			int row[] = new int[N+1];
+			int col[] = new int[M+1];
+			int arr[][] = new int[N+1][M+1];
+
+			int Q = sc.nextInt();
+			for(int j=0;j<Q;j++) {
+				int x = sc.nextInt();
+				int y = sc.nextInt();
+
+				row[x]+=1;
+				col[y]+=1;
+
+			}
+
+			int count = 0;
+			for(int x=1;x<N+1;x++) {
+				for(int y=1;y<M+1;y++) {
+					if((row[x]+col[y])%2!=0)
+						count++;
+				}
+			}
+			System.out.println(count);
+		}
 		}
 
-	}
+		}
+		catch(Exception e){
+		    //ignore
+		}
 
-}
+	}//end of main
+
+}//end of class

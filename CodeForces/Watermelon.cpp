0f3c507489782b2e853970d0414cafdc7bc8d5e3
Pavan Rajkumar <pavanraj14@yahoo.com>
2019-10-09 19:29:32 +0530

    Add Watermelon.cpp

---
+++ CodeForces/Watermelon.cpp
@@ -0,0 +1,14 @@
+//https://codeforces.com/problemset/problem/4/A
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int val;
+    cin >> val;
+    if(val % 2 == 0)
+        cout << "YES";
+    else
+        cout << "NO";
+    return 0;
+}

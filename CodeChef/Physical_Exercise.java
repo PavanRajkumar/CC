b8aca839a5bbc426d0365082ed68d447c4b637be
Pavan Rajkumar <pavanraj14@yahoo.com>
2019-11-04 20:25:44 +0530

    Add incomplete Physical Exercise

---
+++ CodeChef/Physical_Exercise.java
@@ -0,0 +1,38 @@
+//https://www.codechef.com/NOV19B/problems/PHCUL
+
+import java.util.*;
+import java.lang.*;
+
+import java.lang.*;
+	public class CodeForces{
+	    public static void main(String[] args) {
+	    	Scanner sc = new Scanner(System.in);
+	    	int T = sc.nextInt();
+
+	    	for(int t=0;t<T;t++) {
+
+	    		int x = sc.nextInt();
+	    		int y = sc.nextInt();
+	    		int N = sc.nextInt();
+	    		int M = sc.nextInt();
+	    		int K = sc.nextInt();
+
+	    		int Narray[] = new int[N*2];
+	    		int Marray[] = new int[M*2];
+	    		int Karray[] = new int[K*2];
+
+	    		for(int n=0;n<N*2;n++)
+	    			Narray[n] = sc.nextInt();
+	    		for(int m=0;m<M*2;m++)
+	    			Marray[m] = sc.nextInt();
+	    		for(int k=0;k<K*2;k++)
+	    			Karray[k] = sc.nextInt();
+
+	    		for(int i=0;i<N*2-1;i+=2) {
+
+	    		}
+
+
+	    	}
+	    }
+	}

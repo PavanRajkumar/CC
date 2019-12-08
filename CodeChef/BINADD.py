7f12b5c817dc4d3ef27c99904250d3cffda82746
Pavan Rajkumar <pavanraj14@yahoo.com>
2019-12-08 21:48:27 +0530

    Add BINADD

---
+++ CodeChef/BINADD.py
@@ -0,0 +1,16 @@
+t = int(input())
+
+while t > 0:
+    A = input()
+    B = input()
+    a = int(A,2)
+    b = int(B,2)
+    count = 0
+    while b > 0:
+        u = a^b
+        v = a&b
+        a = u
+        b = v*2
+        count = count + 1
+    print(count)
+    t = t - 1

2851515de286f2f832aba02b406d683a6aef7145
Pavan Rajkumar <pavanraj14@yahoo.com>
2019-12-09 14:39:34 +0530

    made it shorter

--- CodeChef/BINXOR.py
+++ CodeChef/BINXOR.py
@@ -1,17 +1,10 @@
 # cook your dish here
-import itertools
+from itertools import permutations
 def f(x, y):
-    # Convert integers to string
-    x, y = str(x), str(y)
-
-    # Pad inputs
-    # x, y = x.zfill(max(len(x), len(y))), y.zfill(max(len(x), len(y)))
-
-
     rv = set()
-    for x_comb in set(''.join(perm_x) for perm_x in itertools.permutations(x)):
-        for y_comb in set(''.join(perm_y) for perm_y in itertools.permutations(y)):
-            rv.add(f"{int(x_comb,2) ^ int(y_comb,2):b}")
+    for x_comb in set(''.join(perm_x) for perm_x in permutations(x)):
+        for y_comb in set(''.join(perm_y) for perm_y in permutations(y)):
+            rv.add(f"{int(x_comb,2) ^ int(y_comb,2)}")
 
 
     if(len(rv)<1000000007):
@@ -23,7 +16,7 @@ def f(x, y):
 t = int(input())
 while t>0:
     n = input()
-    a = input()
-    b = input()
+    a = str(input())
+    b = str(input())
     f(a,b)
     t = t - 1

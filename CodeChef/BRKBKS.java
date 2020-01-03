8f0e5f5853c03d8e80c02b82c9187205e1a5004f
Pavan Rajkumar <pavanraj14@yahoo.com>
2020-01-03 16:30:02 +0530

    Add BRKBKS

---
+++ CodeChef/BRKBKS.java
@@ -0,0 +1,35 @@
+//https://www.codechef.com/JAN20B/problems/BRKBKS
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int t=0;t<T;t++) {
+			int S = sc.nextInt();
+			int w1 = sc.nextInt();
+			int w2 = sc.nextInt();
+			int w3 = sc.nextInt();
+
+			if(w1+w2+w3<=S)
+				System.out.println(1);
+
+			else if(w1+w2>S) {
+				if(w2+w3<=S)
+					System.out.println(2);
+				else
+					System.out.println(3);
+			}
+
+			else
+				System.out.println(2);
+		}
+	}
+}
